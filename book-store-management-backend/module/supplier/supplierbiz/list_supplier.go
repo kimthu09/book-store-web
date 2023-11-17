0694ef14fac06b65b796ff5d8859618ed1f2b0e6
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2023-11-17 11:35:19 +0700

    BSM-74 dev: be for search supplier

---
+++ book-store-management-backend/module/supplier/supplierbiz/list_supplier.go
@@ -0,0 +1,42 @@
+package supplierbiz
+
+import (
+	"book-store-management-backend/common"
+	"book-store-management-backend/middleware"
+	"book-store-management-backend/module/supplier/suppliermodel"
+	"context"
+)
+
+type ListSupplierRepo interface {
+	ListSupplier(
+		ctx context.Context,
+		filter *suppliermodel.Filter,
+		paging *common.Paging,
+	) ([]suppliermodel.Supplier, error)
+}
+
+type listSupplierBiz struct {
+	repo      ListSupplierRepo
+	requester middleware.Requester
+}
+
+func NewListSupplierRepo(
+	repo ListSupplierRepo,
+	requester middleware.Requester) *listSupplierBiz {
+	return &listSupplierBiz{repo: repo, requester: requester}
+}
+
+func (biz *listSupplierBiz) ListSupplier(
+	ctx context.Context,
+	filter *suppliermodel.Filter,
+	paging *common.Paging) ([]suppliermodel.Supplier, error) {
+	if !biz.requester.IsHasFeature(common.SupplierViewFeatureCode) {
+		return nil, suppliermodel.ErrSupplierViewNoPermission
+	}
+
+	result, err := biz.repo.ListSupplier(ctx, filter, paging)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
---
+++ book-store-management-backend/module/supplier/supplierrepo/list_supplier.go
@@ -0,0 +1,41 @@
+package supplierrepo
+
+import (
+	"book-store-management-backend/common"
+	"book-store-management-backend/module/supplier/suppliermodel"
+	"context"
+)
+
+type ListSupplierStore interface {
+	ListSupplier(
+		ctx context.Context,
+		filter *suppliermodel.Filter,
+		propertiesContainSearchKey []string,
+		paging *common.Paging,
+	) ([]suppliermodel.Supplier, error)
+}
+
+type listSupplierRepo struct {
+	store ListSupplierStore
+}
+
+func NewListSupplierRepo(store ListSupplierStore) *listSupplierRepo {
+	return &listSupplierRepo{store: store}
+}
+
+func (repo *listSupplierRepo) ListSupplier(
+	ctx context.Context,
+	filter *suppliermodel.Filter,
+	paging *common.Paging) ([]suppliermodel.Supplier, error) {
+	result, err := repo.store.ListSupplier(
+		ctx,
+		filter,
+		[]string{"id", "name", "email", "phone"},
+		paging)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
