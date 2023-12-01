02fc0a6c7447980d8377f2edf7a24d886d09999e
Quoc, Bui Vi <bvquoc.dev@gmail.com>
2023-12-02 01:33:08 +0700

    update sql script

---
+++ sql/03-init-Role.sql
@@ -0,0 +1,3 @@
+INSERT INTO bookstoremanagement.Role (id, name) VALUES ('admin', 'user');
+INSERT INTO bookstoremanagement.Role (id, name) VALUES ('fl_9lf4Ig', 'haha');
+INSERT INTO bookstoremanagement.Role (id, name) VALUES ('user', 'user');
