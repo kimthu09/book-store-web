7d7226858c88cd468aa6c069830ceea533813c57
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2023-12-14 15:20:16 +0700

    update: add groupName for feature

--- book-store-management-backend/module/feature/featuremodel/feature.go
+++ book-store-management-backend/module/feature/featuremodel/feature.go
@@ -8,6 +8,7 @@ import (
 type Feature struct {
 	Id          string `json:"id" gorm:"column:id;" example:"feature id"`
 	Description string `json:"description" gorm:"column:description;" example:"Xem nhân viên"`
+	GroupName   string `json:"groupName" gorm:"column:groupName" example:"Nhân viên"`
 }
 
 func (*Feature) TableName() string {
