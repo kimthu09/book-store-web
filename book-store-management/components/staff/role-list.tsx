627a0499b90c5bb0fabc81d687d2eee96ab1b837
kimthu09 <21521495@gm.uit.edu.vn>
2023-12-17 15:22:41 +0700

    dev: role manage ui

---
+++ book-store-management/components/staff/role-list.tsx
@@ -0,0 +1,72 @@
+import { RoleListProps } from "@/types";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "../ui/command";
+import { LuCheck, LuChevronsUpDown } from "react-icons/lu";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import getAllRole from "@/lib/staff/getAllRole";
+import Loading from "../loading";
+
+const RoleList = ({ role, setRole }: RoleListProps) => {
+  const [openRole, setOpenRole] = useState(false);
+  const { roles, isLoading, isError } = getAllRole();
+
+  if (isError) return <div>Failed to load</div>;
+  if (!roles) {
+    <Loading />;
+  } else
+    return (
+      <DropdownMenu open={openRole} onOpenChange={setOpenRole}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={openRole}
+            className="justify-between w-full"
+          >
+            {role
+              ? roles.find((item) => item.id === role)?.name
+              : "Chọn phân quyền"}
+            <LuChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className=" DropdownMenuContent">
+          <Command>
+            <CommandGroup>
+              {roles.map((item) => (
+                <CommandItem
+                  value={item.name}
+                  key={item.id}
+                  onSelect={() => {
+                    setRole(item.id);
+                    setOpenRole(false);
+                  }}
+                >
+                  <LuCheck
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      item.id === role ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+};
+
+export default RoleList;
