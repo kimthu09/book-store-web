cce79a2aa249423ac226819dd2a69a5dd0b09942
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2023-12-17 22:35:39 +0700

    dev: refresh token

---
+++ book-store-management-backend/module/user/usertransport/ginuser/refresh_token.go
@@ -0,0 +1,41 @@
+package ginuser
+
+import (
+	"book-store-management-backend/common"
+	"book-store-management-backend/component/appctx"
+	"book-store-management-backend/component/tokenprovider/jwt"
+	"book-store-management-backend/module/user/userbiz"
+	"book-store-management-backend/module/user/usermodel"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// @BasePath /v1
+// @Security BearerAuth
+// @Summary Refresh token
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param refreshToken body usermodel.ReqRefreshToken true "refreshToken"
+// @Response 200 {object} usermodel.AccountWithoutRefresh "user token"
+// @Response 400 {object} common.AppError "error"
+// @Router /refreshToken [post]
+func RefreshToken(appCtx appctx.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var data usermodel.ReqRefreshToken
+		if err := c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, common.ErrInvalidRequest(err))
+		}
+
+		tokenProvider := jwt.NewTokenJWTProvider(appCtx.GetSecretKey())
+
+		business := userbiz.NewRefreshTokenBiz(appCtx, 60*60*24*30, tokenProvider)
+		account, err := business.RefreshToken(c.Request.Context(), &data)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(account))
+	}
+}
