c615ca0fc23e2b373ad1140812e257cda8558bdc
kimthu09 <21521495@gm.uit.edu.vn>
2023-12-19 02:42:30 +0700

    fix: book filter

--- book-store-management/components/book-manage/table-layout.tsx
+++ book-store-management/components/book-manage/table-layout.tsx
@@ -8,9 +8,22 @@ const TableLayout = async ({
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
   const page = searchParams["page"] ?? "1";
-
+  const minSellPrice = searchParams["minSellPrice"] ?? undefined;
+  const maxSellPrice = searchParams["maxSellPrice"] ?? undefined;
+  const search = searchParams["search"] ?? undefined;
+  const categories = searchParams["categories"] ?? undefined;
+  const authors = searchParams["authors"] ?? undefined;
+  const publisher = searchParams["publisher"] ?? undefined;
   const booksData: Promise<{ paging: PagingProps; data: Book[] }> = getAllBooks(
-    Number(page)
+    {
+      page: +page,
+      maxSellPrice: maxSellPrice?.toString(),
+      minSellPrice: minSellPrice?.toString(),
+      categoryIds: categories?.toString(),
+      authorIds: authors?.toString(),
+      publisher: publisher?.toString(),
+      search: search?.toString(),
+    }
   );
   const books = await booksData;
   const totalPage = Math.ceil(books.paging.total / books.paging.limit);
