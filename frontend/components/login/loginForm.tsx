1d725954e53c45594b6b5dc90c33c0d11f8e80e9
vuhoang-gr <21520243@gm.uit.edu.vn>
2023-12-30 17:22:02 +0700

    show toast

--- frontend/components/login/loginForm.tsx
+++ frontend/components/login/loginForm.tsx
@@ -7,14 +7,24 @@ import { Button } from "../ui/button";
 import { Checkbox } from "../ui/checkbox";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useToast } from "../ui/use-toast";
 
 const LoginForm = () => {
     const [state, formAction] = useFormState(login, undefined)
     const [isRemember, setIsRemember] = useState(false)
+    const [toastTrigger, setToastTrigger] = useState(false)
+    const { toast } = useToast()
+    useEffect(() => {
+        if (state?.error)
+            toast({
+                title: "Đăng nhập thất bại",
+                description: "Vui lòng kiểm tra lại email và mật khẩu của bạn",
+            });
 
+    }, [state])
 
     return (
-        <form action={formAction}>
+        <form onSubmit={() => setToastTrigger(!toastTrigger)} action={formAction}>
             <div className={styles.inputContainer}>
                 <div className={styles.rightHeader} >
                     <div className='flex flex-col gap-1'>
