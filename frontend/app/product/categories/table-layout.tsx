5250f7b753a7f5e740e518cb7bf215a856d342b2
kimthu09 <21521495@gm.uit.edu.vn>
2024-01-02 00:24:40 +0700

    fix: create category, author by file

--- frontend/app/product/categories/table-layout.tsx
+++ frontend/app/product/categories/table-layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { CategoryTable } from "@/components/book-manage/category-table";
 import CreateCategory from "@/components/book-manage/create-category";
+import ImportSheet from "@/components/book-manage/import-sheet";
 import Loading from "@/components/loading";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
 import { endPoint } from "@/constants";
+import createListCategory from "@/lib/book/createListCategory";
 import { useRouter } from "next/navigation";
 import { Suspense } from "react";
 import { useSWRConfig } from "swr";
@@ -18,15 +21,57 @@ const TableLayout = ({
   const router = useRouter();
   const page = searchParams["page"] ?? "1";
 
-  const handleCategoryAdded = (idAuthor: string) => {
+  const handleCategoryAdded = (name: string) => {
     mutate(`${endPoint}/v1/categories?page=${page ?? 1}&limit=10`);
-    router.refresh();
+  };
+  const handleFile = async (reader: FileReader) => {
+    let categories: string[] = [];
+    const ExcelJS = require("exceljs");
+    const wb = new ExcelJS.Workbook();
+    reader.onload = () => {
+      const buffer = reader.result;
+      wb.xlsx.load(buffer).then((workbook: any) => {
+        workbook.eachSheet((sheet: any, id: any) => {
+          sheet.eachRow((row: any, rowIndex: number) => {
+            if (rowIndex > 1) {
+              const name = row.getCell(1).value.toString();
+              if (name && name != "") {
+                categories.push(name);
+              }
+            }
+          });
+        });
+      });
+    };
+
+    console.log(categories);
+    const response: Promise<any> = createListCategory({ names: categories });
+    const responseData = await response;
+    console.log(responseData);
+    if (responseData.hasOwnProperty("errorKey")) {
+      toast({
+        variant: "destructive",
+        title: "Có lỗi",
+        description: responseData.message,
+      });
+    } else {
+      toast({
+        variant: "success",
+        title: "Thành công",
+        description: "Thêm thể loại thành công",
+      });
+      handleCategoryAdded("");
+    }
   };
   return (
     <div className="col">
       <div className="flex flex-row justify-between items-center">
         <h1>Thể loại</h1>
         <div className="flex gap-4">
+          <ImportSheet
+            handleFile={handleFile}
+            sampleFileLink="/category-sample.xlsx"
+          />
           <CreateCategory handleCategoryAdded={handleCategoryAdded}>
             <Button>Thêm thể loại</Button>
           </CreateCategory>
---
+++ frontend/components/book-manage/import-sheet.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import { FiUpload } from "react-icons/fi";
+import { toast } from "../ui/use-toast";
+
+const ImportSheet = ({
+  handleFile,
+  sampleFileLink,
+}: {
+  handleFile: (reader: FileReader) => void;
+  sampleFileLink: string;
+}) => {
+  const [file, setFile] = useState<any>();
+  const fileType = [
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  ];
+  const handleChange = (e: any) => {
+    let selectedFile = e.target.files[0];
+    if (selectedFile) {
+      if (selectedFile && fileType.includes(selectedFile.type)) {
+        if (selectedFile.size > 2000000) {
+          console.log(e.target.files[0].size);
+          console.log("Dung lượng file không hợp lệ");
+          toast({
+            variant: "destructive",
+            title: "Có lỗi",
+            description: "Dung lượng file không hợp lệ",
+          });
+        } else {
+          setFile(null);
+          setFile(e.target.files[0]);
+        }
+      } else {
+        setFile(null);
+        console.log(selectedFile.type);
+
+        toast({
+          variant: "destructive",
+          title: "Có lỗi",
+          description: "File không hợp lệ",
+        });
+      }
+    }
+  };
+  const handleImport = () => {
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+
+    reader.readAsArrayBuffer(file);
+    handleFile(reader);
+  };
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant={"outline"} className="bg-white px-3">
+          <div className="flex flex-nowrap gap-1 items-center">
+            <FiUpload className="w-4 h-4 text-green-700" />
+            Nhập danh sách
+          </div>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"top"} className="w-[480px] sm:w-[540px] m-auto">
+        <SheetHeader>
+          <SheetTitle>Nhập danh sách</SheetTitle>
+        </SheetHeader>
+        <div className="text-sm text-muted-foreground mt-4">
+          <p>- Chuyển đổi file nhập dưới dạng .XLSX trước khi tải dữ liệu</p>
+          <span>
+            - Tải file mẫu
+            <Button variant={"link"} className="px-1">
+              <a href={sampleFileLink} download="Sample.xlsx">
+                tại đây
+              </a>
+            </Button>
+          </span>
+          <p>- File nhập có dung lượng tối đa là 2MB.</p>
+        </div>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Input
+              id="file"
+              type="file"
+              onChange={handleChange}
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <div className="gap-3 flex">
+              <Button variant={"outline"}>Hủy</Button>
+              <Button type="button" onClick={handleImport}>
+                Nhập file
+              </Button>
+            </div>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default ImportSheet;
