98e06e5270f32243804950c042179a8483211dbb
kimthu09 <21521495@gm.uit.edu.vn>
2024-01-14 22:55:37 +0700

    dev: simple skeleton

---
+++ frontend/components/skeleton/dropdown-skeleton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Skeleton } from "../ui/skeleton";
+
+const DropdownSkeleton = () => {
+  return <Skeleton className="h-10 w-full" />;
+};
+
+export default DropdownSkeleton;
