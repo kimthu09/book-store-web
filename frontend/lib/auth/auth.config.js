1196e5ecbda6dc0bfec4068fba993338fb8ac8c1
bvquoc <bvquoc.dev@gmail.com>
2024-06-04 22:02:04 +0700

    fix version

--- frontend/lib/auth/auth.config.js
+++ frontend/lib/auth/auth.config.js
@@ -1,40 +1,42 @@
+import { NextResponse } from "next/server";
+
 export const authConfig = {
-    pages: {
-      signIn: "/login",
-      forgotPassword: "/forgot-password",
+  pages: {
+    signIn: "/login",
+    forgotPassword: "/forgot-password",
+  },
+  providers: [],
+  callbacks: {
+    jwt({ token, user }) {
+      // console.log(user)
+      if (user) {
+        token.id = user;
+      }
+      return token;
     },
-    providers: [],
-    callbacks: {
-      jwt({ token, user }) {
-        // console.log(user)
-        if (user) {
-          token.id = user;
-        }
-        return token;
-      },
-      session({ session, token }) {
-        if (token) {
-          session.user = token.id;
-        }
-        return session;
-      },
-      async authorized({ auth, request }) {
-        const user = auth?.user;
-        // console.log(user)
-        const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
-        const isOnForgotPasswordPage =
-          request.nextUrl?.pathname.startsWith("/forgot-password");
-  
-        if (user && isOnLoginPage) {
-          return Response.redirect(new URL("/", request.nextUrl));
-        }
-        if (user && isOnForgotPasswordPage) {
-          return Response.redirect(new URL("/", request.nextUrl));
-        }
-        if (!user && !isOnLoginPage && !isOnForgotPasswordPage) {
-          return false;
-        }
-        return true;
-      },
+    session({ session, token }) {
+      if (token) {
+        session.user = token.id;
+      }
+      return session;
     },
-  };
+    async authorized({ auth, request }) {
+      const user = auth?.user;
+      // console.log(user)
+      const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
+      const isOnForgotPasswordPage =
+        request.nextUrl?.pathname.startsWith("/forgot-password");
+
+      if (user && isOnLoginPage) {
+        return NextResponse.redirect(new URL("/", request.nextUrl));
+      }
+      if (user && isOnForgotPasswordPage) {
+        return NextResponse.redirect(new URL("/", request.nextUrl));
+      }
+      if (!user && !isOnLoginPage && !isOnForgotPasswordPage) {
+        return false;
+      }
+      return true;
+    },
+  },
+};
